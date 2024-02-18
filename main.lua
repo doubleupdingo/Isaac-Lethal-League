a244f92b3587de6b33afc2a8b9468929d85aa7cb
doubleupdingo <maferreira@quinnipiac.edu>
2024-02-18 12:58:27 -0500

    the project so far

---
+++ main.lua
@@ -0,0 +1,35 @@
+local MyCharacterMod = RegisterMod("Gabriel Character Mod", 1)
+
+local gabrielType = Isaac.GetPlayerTypeByName("Gabriel", false) -- Exactly as in the xml. The second argument is if you want the Tainted variant.
+local hairCostume = Isaac.GetCostumeIdByPath("gfx/characters/gabriel_hair.anm2") -- Exact path, with the "resources" folder as the root
+local stolesCostume = Isaac.GetCostumeIdByPath("gfx/characters/gabriel_stoles.anm2") -- Exact path, with the "resources" folder as the root
+
+function MyCharacterMod:GiveCostumesOnInit(player)
+    if player:GetPlayerType() ~= gabrielType then
+        return -- End the function early. The below code doesn't run, as long as the player isn't Gabriel.
+    end
+
+    player:AddNullCostume(hairCostume)
+    player:AddNullCostume(stolesCostume)
+end
+
+MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MyCharacterMod.GiveCostumesOnInit)
+
+function MyCharacterMod:RemoveCementCostume(player)
+    if player.FrameCount ~= 1 then
+      return
+    end
+
+    if player and player:GetName() == "Gabriel" then
+        local itemConfig = Isaac.GetItemConfig()
+        local itemConfigItem = itemConfig:GetCollectible(CollectibleType.COLLECTIBLE_RUBBER_CEMENT)
+
+        print("Checking if player type is correct: ", player:GetPlayerType(), gabrielType)
+
+        if itemConfigItem ~= nil then
+            player:RemoveCostume(itemConfigItem)
+        end
+    end
+end
+
+MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, MyCharacterMod.RemoveCementCostume)
