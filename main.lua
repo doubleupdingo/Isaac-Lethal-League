8a2798d19dba0d3478331d0bdd9197330ca5295f
doubleupdingo <maferreira@quinnipiac.edu>
2024-03-31 23:51:56 -0400

    all 5 items functional and in item pools, no costumes

--- main.lua
+++ main.lua
@@ -37,12 +37,63 @@ mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.RemoveCementCostume)
 local jetSkates = Isaac.GetItemIdByName("Jet's Skates")
 local jetSkatesSpeed = 0.4
 
+local switchHat = Isaac.GetItemIdByName("Switch's Hat")
+local switchHatSpeed = 0.2
+local switchHatRange = 80
+
+local ivoryPuppet = Isaac.GetItemIdByName("Ivory Puppet: Retribution")
+
+local ashesAscent = Isaac.GetItemIdByName("Ashes Ascent")
+local ashesAscentDamage = 1
+
+local dicePaddle = Isaac.GetItemIdByName("Dice's Paddle")
+local dicePaddleSpeed = 0.2
+local dicePaddleRange = 120
+local dicePaddleShotSpeed = 0.2
+
 function mod:EvaluateCache(player, cacheFlags)
-    if cacheFlags & CacheFlag.CACHE_SPEED == CacheFlag.CACHE_SPEED then
-        local itemCount = player:GetCollectibleNum(jetSkates)
+    if cacheFlags & CacheFlag.CACHE_SPEED == CacheFlag.CACHE_SPEED then -- Speed cache
+        local itemCount = player:GetCollectibleNum(jetSkates) -- Jet's Skates (speed)
         local speedToAdd = jetSkatesSpeed * itemCount
         player.MoveSpeed = player.MoveSpeed + speedToAdd
+        local itemCount = player:GetCollectibleNum(switchHat) -- Switch's Hat (speed)
+        local speedToAdd = switchHatSpeed * itemCount
+        player.MoveSpeed = player.MoveSpeed + speedToAdd
+        local itemCount = player:GetCollectibleNum(dicePaddle) -- Dice's Paddle (speed)
+        local speedToAdd = dicePaddleSpeed * itemCount
+        player.MoveSpeed = player.MoveSpeed + speedToAdd
+    end
+    if cacheFlags & CacheFlag.CACHE_RANGE == CacheFlag.CACHE_RANGE then -- Range cache
+        local itemCount = player:GetCollectibleNum(switchHat) -- Switch's Hat (range)
+        local rangeToAdd = switchHatRange * itemCount
+        player.TearRange = player.TearRange + rangeToAdd
+        local itemCount = player:GetCollectibleNum(dicePaddle) -- Dice's Paddle (range)
+        local rangeToAdd = dicePaddleRange * itemCount
+        player.TearRange = player.TearRange + rangeToAdd
+    end
+    if cacheFlags & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then -- Damage cache
+        local itemCount = player:GetCollectibleNum(ashesAscent) -- Ashes Ascent (damage)
+        local damageToAdd = ashesAscentDamage * itemCount
+        player.Damage = player.Damage + damageToAdd
+    end
+    if cacheFlags & CacheFlag.CACHE_SHOTSPEED == CacheFlag.CACHE_SHOTSPEED then -- Shot speed cache
+        local itemCount = player:GetCollectibleNum(dicePaddle) -- Dice's Paddle (shot speed)
+        local shotSpeedToAdd = dicePaddleShotSpeed * itemCount
+        player.ShotSpeed = player.ShotSpeed + shotSpeedToAdd
+    end
+    if cacheFlags & CacheFlag.CACHE_FLYING == CacheFlag.CACHE_FLYING then -- Flying cache
+        if player:GetCollectibleNum(ashesAscent) >= 1 then
+            player.CanFly = true
+        end
     end
 end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
+
+function mod:PostAddCollectible(type, charge, FirstTime, slot, VarData, player)
+    if type == 735 or type == 736 then -- Ivory Puppet: Retribution or Ashes Ascent (broken hearts)
+        Isaac.GetPlayer():AddBrokenHearts(1)
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_ADD_COLLECTIBLE, mod.PostAddCollectible)
